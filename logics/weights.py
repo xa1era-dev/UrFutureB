309b1d92281f25368fc8591e734090af9e783444
_luvairo <mihail-shutov@inbox.ru>
2024-04-20 21:06:00 +0500

    Initial algo commit.

---
+++ logics/weights.py
@@ -0,0 +1,7 @@
+from typing import List
+from core.models.models import Course, Tag
+
+
+# TODO: Добавить описание метода.
+def get_course_weight(course: Course, tags: List[Tag], order_matters: bool = False) -> float:
+    pass
